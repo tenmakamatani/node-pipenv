4515a7912614a2af9fb80e796fbfff7a47abc739
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-09-02 01:12:59 +0900

    [update]pipfile scripts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const exec = require("child_process").exec;
 const path = require("path");
 
 const childDir = path.join(process.cwd(), "python");
-exec("pipenv run python main.py 僕の名前は天馬と言います", { cwd: childDir }, (err, stdout, stderr) => {
+exec("pipenv run start 僕の名前は天馬と言います", { cwd: childDir }, (err, stdout, stderr) => {
     if (err) console.log("Error occured", err);
     console.log("Output: ", stdout);
 });
